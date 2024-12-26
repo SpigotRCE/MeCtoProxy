5ddc2a838f7f5338aa7ac2afcdd800d0e0b72561
SpigotRCE <spigotrce@gmail.com>
2024-12-26 12:17:15 +0530

    removal: alias in AbstractCommand

--- src/main/java/io/github/spigotrce/mectoproxy/command/AbstractCommand.java
+++ src/main/java/io/github/spigotrce/mectoproxy/command/AbstractCommand.java
@@ -3,11 +3,5 @@ package io.github.spigotrce.mectoproxy.command;
 import com.velocitypowered.api.command.SimpleCommand.Invocation;
 
 public abstract class AbstractCommand {
-    public final String alias;
-
-    public AbstractCommand(String alias) {
-        this.alias = alias;
-    }
-
     public abstract void execute(Invocation invocation);
 }
